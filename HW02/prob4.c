bd166d8a3ba34f74cc23ee37002458605bb63949
John Kapustka <jkapustk@nd.edu>
2023-01-24 18:17:01 -0500

    moved problem 3 and 4 into HW02 folder

---
+++ HW02/prob4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void overflow(int, int);
+
+int main() {
+
+	overflow(300, 100);
+	overflow(-300, -100);
+	overflow(0x7FFFFFFF, 1);
+	overflow(0x80000000, -1);
+	overflow(0x7FFFFFFF, 0x80000000);
+
+	return 0;
+}
+
+void overflow(int num1, int num2) {
+
+	// numbers overflow if two same-sign ints have an opposite sign sum
+	
+	printf("\n%d\t%x\n", num1+num2, num1+num2);
+
+	int mask = 0xf0000000;
+	int sum = num1+num2;
+	if (((num1&mask) & (num2&mask)) & ~(sum&mask)) printf("overflow\n");
+
+	if (~(num1&mask) & ~(num2&mask) & (sum&mask)) printf("overflow\n");
+
+}
